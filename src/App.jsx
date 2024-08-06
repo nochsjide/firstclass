debb6e91ab47bc424b7d411fbbb7c7356e5fbf95
enoch <nochs1234@gmail.com>
2024-08-06 10:31:33 +0100

    changes made

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Homepage from './components/Homepage'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Homepage/>
+      <Footer/>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className="footer bg-light text-center mt-5 py-3">
+      <div className="container">
+        <span className="text-muted">© 2024 Pizzacle Shop. All rights reserved.</span>
+      </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
